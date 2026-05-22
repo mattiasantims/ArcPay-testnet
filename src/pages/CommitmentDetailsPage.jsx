c38f34983d09f38fae318b80c2e66a7e5cebbab1
mattiasantims <mattia.santi.ms@gmail.com>
2026-05-23 00:17:16 +0200

    fix

--- src/pages/CommitmentDetailsPage.jsx
+++ src/pages/CommitmentDetailsPage.jsx
@@ -3,7 +3,7 @@ import { useParams, useSearchParams } from 'react-router-dom'
 import { useAccount } from 'wagmi'
 import { useWeb3Modal } from '@web3modal/wagmi/react'
 import { QRCodeSVG } from 'qrcode.react'
-import { requestRefund, isRefundContractConfigured } from '../utils/refund.js'
+import { requestRefund } from '../utils/refund.js'
 import {
   fetchCommitment, fulfillDelayedCommitment, fulfillTranche, cancelCommitment,
   COMMITMENT_STATUS_LABEL, COMMITMENT_STATUS_COLOR, COMMITMENT_TYPE_LABEL,
